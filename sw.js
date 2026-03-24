cda22e31612cbb95f085b92ec2c0fa6c167687f5
emmanuelcompanysrl <emmanuelcompany.srl@gmail.com>
2026-03-24 19:04:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,18 +2,16 @@ const CACHE_NAME = 'salvadanaio-v1';
 const ASSETS = [
   '/Salvadanaio/',
   '/Salvadanaio/index.html',
-  '/Salvadanaio/manifest.json',
+  '/Salvadanaio/app.json', // <--- Cambiato qui
   '/Salvadanaio/icona.png'
 ];
 
-// Installazione e salvataggio file
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Recupero file (funziona offline)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
